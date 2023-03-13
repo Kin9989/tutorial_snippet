fd74bcaa17ceb42b34cef2e9c3870923e8bf5f79
Kin9989 <haoqh@int.t4tek.co>
2023-03-13 09:16:22 +0700

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+{
+    "name": "Tutorial Snippet",
+    "summary": """
+        tutorial snippets""",
+    "description": """
+        ok
+    """,
+    "author": "vyngt",
+    "website": "https://github.com/vyngt",
+    "category": "Uncategorized",
+    "version": "16.0.1.0.0",
+    "depends": ["base", "website"],
+    "data": [
+        # 'security/ir.model.access.csv',
+        "views/snippets/v_block.xml",
+        "views/snippets/snippets.xml",
+        #  Nó ngay ở dưới , IT HERE
+        "views/snippets/trung_block.xml",
+        "views/snippets/cart_flip.xml",
+
+        
+    ],
+}
